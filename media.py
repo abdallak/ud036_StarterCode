f64d346d4211a51f24100ef14227ade7e08bef09
abdallak <32675031+abdallak@users.noreply.github.com>
2018-03-28 17:05:51 +0200

    just better

--- media.py
+++ media.py
@@ -1,11 +1,6 @@
 class Movie():
-    def __init__ (self, movie_title, poster_image, trailer_youtube):
+    """ this Class encapsulat the properties of a movie in a movie object"""
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
-
-
-
-
-
